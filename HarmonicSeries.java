950eda2c0774964955830da753d77144a4e3a386
Mert Talug <merttalug14@gmail.com>
2022-03-11 18:31:52 +0300

    Create HarmonicSeries.java

---
+++ HarmonicSeries.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class HarmonicSeries {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n;
+        boolean askn = false;
+        double sum = 0;
+
+        do {
+            System.out.println("Please enter the number n: ");
+            n = input.nextInt();
+            if (n >= 0) {
+                askn = true;
+            }
+        }
+        while (!askn);
+
+        for (double i = 1; i <= n; i++) {
+            sum += (1 / i);
+        }
+
+        System.out.println(sum);
+
+    }
+}
